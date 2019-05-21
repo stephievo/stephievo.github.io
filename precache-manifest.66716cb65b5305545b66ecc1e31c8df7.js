9c06a03c2a0d4f9aa296d80e768a51d7006a7ce0
Vo Stephanie <vo.stephie@gmail.com>
2019-05-22 01:30:56 +0200

    Updates

---
+++ precache-manifest.66716cb65b5305545b66ecc1e31c8df7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "58faa7004522242b14c7",
+    "url": "/static/css/main.c83f879b.chunk.css"
+  },
+  {
+    "revision": "58faa7004522242b14c7",
+    "url": "/static/js/main.58faa700.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b755cb8e02f8d5fa1ae6",
+    "url": "/static/css/2.8f28b1c9.chunk.css"
+  },
+  {
+    "revision": "b755cb8e02f8d5fa1ae6",
+    "url": "/static/js/2.b755cb8e.chunk.js"
+  },
+  {
+    "revision": "a2dcadc06d8a3607f1561f937c1a0f61",
+    "url": "/static/media/DepressionBanner.a2dcadc0.png"
+  },
+  {
+    "revision": "3b6fc18d273288dfc4d71ba7b6e0983a",
+    "url": "/static/media/LFGbanner.3b6fc18d.png"
+  },
+  {
+    "revision": "0562f62107dbeb5b79b910a5cfec19a3",
+    "url": "/static/media/TipTapBanner2.0562f621.png"
+  },
+  {
+    "revision": "05b05b9b0683cf192b7f56ec0261c4ef",
+    "url": "/static/media/hacknext.05b05b9b.jpg"
+  },
+  {
+    "revision": "73e18e2dbc3c18ce8de7fec00124b1e3",
+    "url": "/static/media/PrototypeScreen.73e18e2d.png"
+  },
+  {
+    "revision": "ed8f383131c5ef9a8268bd4139a669e8",
+    "url": "/static/media/depressionsketch.ed8f3831.JPG"
+  },
+  {
+    "revision": "9a3c75c4bf4438b6b312da658892a6bc",
+    "url": "/static/media/allornothing.9a3c75c4.png"
+  },
+  {
+    "revision": "76f541d51613587455d65bc46e7322fc",
+    "url": "/static/media/mentalfilter.76f541d5.png"
+  },
+  {
+    "revision": "e236ebd4c060c502c6ccfe51a9906815",
+    "url": "/static/media/emotionalreasoning.e236ebd4.png"
+  },
+  {
+    "revision": "b78099f2bbda002390ea568487c85a47",
+    "url": "/static/media/disqualifying.b78099f2.png"
+  },
+  {
+    "revision": "b5a4a5c098faaeca784c0c489ec4d73a",
+    "url": "/static/media/jumping.b5a4a5c0.png"
+  },
+  {
+    "revision": "50803a9c7d14bf8e9e7121a0007cb81a",
+    "url": "/static/media/overgeneralization.50803a9c.png"
+  },
+  {
+    "revision": "7e959b713e046464fe951f30acde18da",
+    "url": "/static/media/labeling.7e959b71.png"
+  },
+  {
+    "revision": "db8f77cdbbd0c98b67e628d8417b2211",
+    "url": "/static/media/should.db8f77cd.png"
+  },
+  {
+    "revision": "3ff3ac2dbbce61fd3f0e708a18bd11fb",
+    "url": "/static/media/personalization.3ff3ac2d.png"
+  },
+  {
+    "revision": "e540a6534463e98b941c0fdf49e36f1c",
+    "url": "/static/media/maxmin.e540a653.png"
+  },
+  {
+    "revision": "3cd3cf430155b5e279bf53cd30ae4352",
+    "url": "/static/media/lfggoal1.3cd3cf43.png"
+  },
+  {
+    "revision": "11823c901956f033ca14a9aa70ad8e3f",
+    "url": "/static/media/lfggoal3.11823c90.png"
+  },
+  {
+    "revision": "54199e0c8a8f03f8541b00273847ffe4",
+    "url": "/static/media/cardsorting.54199e0c.jpg"
+  },
+  {
+    "revision": "e14d9845bea0b6f93a343b1da40f2ae6",
+    "url": "/static/media/lfgwireframes.e14d9845.jpg"
+  },
+  {
+    "revision": "5c1fa5db1f2e97f2195930d9115d380b",
+    "url": "/static/media/comparison.5c1fa5db.png"
+  },
+  {
+    "revision": "f1cabe8e2705ba9daa75bd8354a85b57",
+    "url": "/index.html"
+  }
+];
